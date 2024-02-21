66fa55f9b9687b9268a788ed5de6f6608b47925d
Lee Felizio <feliziolee@gmail.com>
2024-02-21 13:00:42 +0100

    Contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+export function Contact() {
+    return <>
+        <div>
+            <h2>blablablabllba</h2>
+            blablablabllba
+            dfsd@sfd.com  balb
+        </div>
+    </>
+}
