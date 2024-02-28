80a9d0ce8b995a080b06d9d4a4dc34e4f02b6675
Lee Felizio <feliziolee@gmail.com>
2024-02-28 13:19:59 +0100

    index.html and src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { InsertContent } from './pages/InsertContent.jsx';
-import { Navigation } from './components/Navigation.jsx';
-import { Home } from './pages/Home.jsx';
-import { Contact } from './pages/Contact.jsx';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { InsertContent } from "./pages/InsertContent.jsx";
+import { Navigation } from "./components/Navigation.jsx";
+import { Home } from "./pages/Home.jsx";
+import { Contact } from "./pages/Contact.jsx";
+import { SearchResult } from "./components/search/SearchResult.jsx";
+// import SearchResultItem from './components/SearchResultItem.jsx';
+import SingleResource from "./pages/SingleResource.jsx";
+import { GroupesDiscussion } from "./pages/GroupesDiscussion.jsx";
+import { HealthProfessionals } from "./pages/HealthProfessionals.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Navigation />,
     children: [
-      { path: "", element: <Home />},
-      // login
-
-      { path: "insert", element: <InsertContent />},
-      { path: "contact", element: <Contact />},
-    ]
-    // TO DO - add single resource
+      { path: "", element: <Home /> },
+      { path: "insert", element: <InsertContent /> },
+      { path: "contact", element: <Contact /> },
+      { path: "resource", element: <SearchResult /> },
+      { path: "resource/:id", element: <SingleResource /> },
+      { path: "health-professionals", element: <HealthProfessionals /> },
+      { path: "support-groupes", element: <GroupesDiscussion /> },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
