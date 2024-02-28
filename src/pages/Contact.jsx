a3483c5d5f581ae4c8e2ecb0d0f853cf01fb4e28
Lee Felizio <feliziolee@gmail.com>
2024-02-28 13:30:57 +0100

    Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom";
+
 export function Contact() {
-    return <>
+  return (
+    <div className="bg-grad-orange-washed tw-min-h-screen tw-text-slate-900">
+      <div className="page-container">
+        <h2 className="page-title">Contacter</h2>
+
         <div>
-            <h2>blablablabllba</h2>
-            blablablabllba
-            dfsd@sfd.com  balb
+          <p className="tw-text-lg tw-font-bold">
+            Please, feel free to contact us anytime.
+          </p>
+          <div>
+            <Link
+              to={`mailto:hello@communityresources.com`}
+              className="tw-text-2xl tw-font-bold tw-text-slate-900"
+            >
+              hello@communityresources.com
+            </Link>
+          </div>
         </div>
-    </>
+      </div>
+    </div>
+  );
 }
